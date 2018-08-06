58c1a7d391acc7cc6d1763ba215f0d16ff8550e5
Priscilla Okawa <prisoka@gmail.com>
2018-08-06 16:56:57 -0700

    wip: dogs route

--- routes/dogs.js
+++ routes/dogs.js
@@ -36,6 +36,31 @@ router.get('/:dogid', (req, res, next) => {
 })
 
 // CREATE one dog
+router.post('/', (req, res, next) => {
+  let dog_name = req.body.dog_name;
+  let dog_age = req.body.dog_age;
+  let dog_size = req.body.dog_size;
+
+
+  knex('dogs')
+  .insert({
+    user_id: 1,
+    dog_name: dog_name,
+    dog_age: dog_age,
+    dog_size: dog_size,
+
+  })
+  .returning('*')
+  .then((result) => {
+    let insertedRecord = result[0]
+    res.send(insertedRecord)
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+
 // router.post('/', () => {
 //   let user_id: ;
 //   let dog_name: req.body.dog_name;
