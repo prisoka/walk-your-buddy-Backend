b16ec3d26b2f9a158bd15c42f1cc9f6c01c7d944
Priscilla Okawa <prisoka@gmail.com>
2018-08-09 13:39:27 -0700

    seeded requests table

--- migrations/20180803141042_requests.js
+++ migrations/20180803141042_requests.js
@@ -4,8 +4,8 @@ exports.up = function(knex, Promise) {
     table.increments().primary();
     table.integer('user_id').references('users.id').onDelete('CASCADE').index().notNullable();
     table.integer('dog_id').references('dogs.id').onDelete('CASCADE').index();
-    table.date('request_date').notNullable();
-    table.time('request_time').notNullable();
+    table.date('request_date');
+    table.time('request_time');
     table.integer('walker_id').references('users.id').onDelete('CASCADE').index();
     table.timestamps(true, true);
   })
---
+++ seeds/003_requests.js
@@ -0,0 +1,30 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('requests').del()
+    .then( () => {
+      // Inserts seed entries
+      return knex('requests').insert([
+        {
+          user_id: 1,
+          dog_id: 1,
+          request_date: '2018/08/09',
+          request_time: '09:00',
+          walker_id: 2,
+        },
+        {
+          user_id: 1,
+          dog_id: 2,
+          request_date: '2018/08/09',
+          request_time: '08:00',
+          walker_id: 2,
+        },
+      ]);
+    });
+};
+
+// id
+// user_id
+// dog_id
+// request_date
+// request_time
+// walker_id
