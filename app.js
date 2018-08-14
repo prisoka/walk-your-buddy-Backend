4eb51c546f9fc161d09f3813cd476a77acb837ab
Priscilla Okawa <prisoka@gmail.com>
2018-08-13 18:01:38 -0700

    allowed methods on cors

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3001");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
