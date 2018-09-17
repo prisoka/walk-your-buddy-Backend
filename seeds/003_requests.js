ba6ab0f76d0dbecade061dbc96892b457c56e79f
Priscilla Okawa <prisoka@gmail.com>
2018-09-16 19:46:49 -0700

    changed seed

--- seeds/003_requests.js
+++ seeds/003_requests.js
@@ -8,9 +8,7 @@ exports.seed = function(knex, Promise) {
           user_id: 1,
           dog_id: 1,
           request_date: '2018/10/09',
-          request_time: '09:00 am',
-          start_walk_time: '2018-10-09 09:00:18.680108-07',
-          finish_walk_time: '2018-10-09 09:30:18.680108-07',
+          request_time: '09:15 am',
           walker_id: 2,
         },
         {
@@ -38,7 +36,7 @@ exports.seed = function(knex, Promise) {
           user_id: 1,
           dog_id: 5,
           request_date: '2019/02/02',
-          request_time: '09:00 am',
+          request_time: '08:00 pm',
           walker_id: null,
         },
         {
@@ -51,7 +49,7 @@ exports.seed = function(knex, Promise) {
         {
           user_id: 1,
           dog_id: 7,
-          request_date: '2018/12/28',
+          request_date: '2018/10/22',
           request_time: '03:00 pm',
           walker_id: null,
         },
@@ -66,7 +64,7 @@ exports.seed = function(knex, Promise) {
           user_id: 1,
           dog_id: 9,
           request_date: '2019/01/15',
-          request_time: '10:45 am',
+          request_time: '06:45 am',
           walker_id: null,
         },
         {
@@ -94,7 +92,7 @@ exports.seed = function(knex, Promise) {
           user_id: 1,
           dog_id: 13,
           request_date: '2019/03/05',
-          request_time: '10:05 am',
+          request_time: '10:15 am',
           walker_id: null,
         }
       ]);
