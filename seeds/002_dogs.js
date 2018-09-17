8e8efef359fb02a8bba0465b4a6518f4ffb507aa
Priscilla Okawa <prisoka@gmail.com>
2018-09-16 19:15:02 -0700

    seeded with more dogs and requests

--- seeds/002_dogs.js
+++ seeds/002_dogs.js
@@ -39,6 +39,69 @@ exports.seed = function(knex, Promise) {
           dog_age: 2,
           dog_size: 'small',
           dog_photo_url: process.env.REACT_APP_API_URL + '/images/mink-mingle-323378-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Rolls',
+          dog_age: 7,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/aaron-andary-358414-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Oreo',
+          dog_age: 5,
+          dog_size: 'medium',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/baptist-standaert-346832-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Buddy',
+          dog_age: 1,
+          dog_size: 'small',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/berkay-gumustekin-402114-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Rex',
+          dog_age: 3,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/courtney-roberson-621693-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Jon Snow',
+          dog_age: 4,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/marek-szturc-508923-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Mia',
+          dog_age: 5,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/mia-phoy-558468-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Goofy',
+          dog_age: 3,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/reuben-mcfeeters-351103-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Caramel',
+          dog_age: 5,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/ryan-walton-520633-unsplash.jpg'
+        },
+        {
+          user_id: 1,
+          dog_name: 'Soneca',
+          dog_age: 4,
+          dog_size: 'large',
+          dog_photo_url: process.env.REACT_APP_API_URL + '/images/valerie-elash-690673-unsplash.jpg'
         }
       ]);
     });
